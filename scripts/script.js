c802ca1d4df2b2d8092a32ff31ab546adce035da
Steve <sgriesel@gmail.com>
2019-04-16 06:33:07 +0200

    add fade in titles, small style tweaks

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,34 @@ function scrollToTop() {
 	}
 	else clearTimeout(timeOut);
 }
+
+// Fade in elements
+var fadeIn = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top; // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+        if (positionFromTop - windowHeight <= 0) {
+          elems[i].className = elems[i].className.replace(
+            'hidden',
+            'fade-in-element'
+          );
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  fadeIn().init();
